6508435458152fc2fb29a729f6dc601489feca62
Baiborin Petr <razor267@mail.ru>
2022-06-08 20:09:09 +0300

    remove all trash from project, create project structure

---
+++ src/pages/Table/Table.tsx
@@ -0,0 +1,12 @@
+import React, {FC} from 'react'
+import s from './Table.module.css'
+
+const Table: FC = () => {
+    return (
+        <div>
+            Hello World
+        </div>
+    )
+}
+
+export default Table
